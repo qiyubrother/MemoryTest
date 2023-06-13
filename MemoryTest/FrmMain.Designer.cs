a489dc797e00bc8fd612a791012c96e144e7f186
nnd_liuzhenhua <liuzhenhua@nndrobot.com>
2023-06-13 13:30:27 +0800

    0

--- MemoryTest/FrmMain.Designer.cs
+++ MemoryTest/FrmMain.Designer.cs
@@ -28,103 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
-            this.txtMemoryAllowed = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtAllowVirtualMemorySize64 = new System.Windows.Forms.TextBox();
-            this.btnBegin = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // txtMemoryAllowed
-            // 
-            this.txtMemoryAllowed.Location = new System.Drawing.Point(167, 12);
-            this.txtMemoryAllowed.Name = "txtMemoryAllowed";
-            this.txtMemoryAllowed.Size = new System.Drawing.Size(100, 21);
-            this.txtMemoryAllowed.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "已分配内存";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(72, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "已分配虚拟内存";
-            // 
-            // txtAllowVirtualMemorySize64
-            // 
-            this.txtAllowVirtualMemorySize64.Location = new System.Drawing.Point(167, 39);
-            this.txtAllowVirtualMemorySize64.Name = "txtAllowVirtualMemorySize64";
-            this.txtAllowVirtualMemorySize64.Size = new System.Drawing.Size(100, 21);
-            this.txtAllowVirtualMemorySize64.TabIndex = 3;
-            // 
-            // btnBegin
-            // 
-            this.btnBegin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnBegin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBegin.Location = new System.Drawing.Point(273, 12);
-            this.btnBegin.Name = "btnBegin";
-            this.btnBegin.Size = new System.Drawing.Size(120, 21);
-            this.btnBegin.TabIndex = 0;
-            this.btnBegin.Text = "开始测试";
-            this.btnBegin.UseVisualStyleBackColor = true;
-            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::MemoryTest.Properties.Resources.Memory;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.label3.Location = new System.Drawing.Point(274, 40);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 19);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Any CPU | 首选32位";
-            // 
-            // FrmMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 74);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtAllowVirtualMemorySize64);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtMemoryAllowed);
-            this.Controls.Add(this.btnBegin);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FrmMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Memory Allow Test for .net win32 application";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+			this.txtMemoryAllowed = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtAllowVirtualMemorySize64 = new System.Windows.Forms.TextBox();
+			this.btnBegin = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtMemoryAllowed
+			// 
+			this.txtMemoryAllowed.Location = new System.Drawing.Point(167, 12);
+			this.txtMemoryAllowed.Name = "txtMemoryAllowed";
+			this.txtMemoryAllowed.Size = new System.Drawing.Size(100, 21);
+			this.txtMemoryAllowed.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(96, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "已分配内存";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(72, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(89, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "已分配虚拟内存";
+			// 
+			// txtAllowVirtualMemorySize64
+			// 
+			this.txtAllowVirtualMemorySize64.Location = new System.Drawing.Point(167, 39);
+			this.txtAllowVirtualMemorySize64.Name = "txtAllowVirtualMemorySize64";
+			this.txtAllowVirtualMemorySize64.Size = new System.Drawing.Size(100, 21);
+			this.txtAllowVirtualMemorySize64.TabIndex = 3;
+			// 
+			// btnBegin
+			// 
+			this.btnBegin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnBegin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnBegin.Location = new System.Drawing.Point(273, 12);
+			this.btnBegin.Name = "btnBegin";
+			this.btnBegin.Size = new System.Drawing.Size(120, 21);
+			this.btnBegin.TabIndex = 0;
+			this.btnBegin.Text = "开始测试";
+			this.btnBegin.UseVisualStyleBackColor = true;
+			this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::MemoryTest.Properties.Resources.Memory;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label3.Font = new System.Drawing.Font("微软雅黑", 9F);
+			this.label3.Location = new System.Drawing.Point(274, 40);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(120, 19);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Any CPU | 首选32位";
+			// 
+			// FrmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(407, 74);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtAllowVirtualMemorySize64);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtMemoryAllowed);
+			this.Controls.Add(this.btnBegin);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FrmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Memory Allow Test for .net win32 application";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
